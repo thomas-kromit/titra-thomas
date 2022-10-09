578feb187a7771d8884b44870672185f1c050fa8
Thomas Leb <thomas@kromit.at>
2022-10-09 23:11:59 +0200

    change timeunit on the fly function

--- imports/ui/components/navbar.js
+++ imports/ui/components/navbar.js
@@ -1,9 +1,10 @@
 import { Template } from 'meteor/templating'
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra'
-import { displayUserAvatar, getUserSetting } from '../../utils/frontend_helpers'
-
+import { t } from '../../utils/i18n.js'
+import { displayUserAvatar, getUserSetting, showToast } from '../../utils/frontend_helpers'
 import './navbar.html'
 import './timetracker'
+
 
 Template.navbar.onCreated(function navbarCreated() {
   this.autorun(() => {
@@ -18,4 +19,19 @@ Template.navbar.helpers({
   displayLinkText: (routename) => (FlowRouter.getRouteName() === routename),
   avatar: () => displayUserAvatar(Meteor.user()),
   getUserSetting: (setting) => getUserSetting(setting),
+})
+
+
+Template.navbar.events({
+  'click .timeunitSwitch': (event, templateInstance) => {
+    event.preventDefault()
+    if (getUserSetting('timeunit') === 'd') {
+      showToast(t('settings.time_unit') + ': ' + (t('globals.hour_plural')))
+    } else if (getUserSetting('timeunit') === 'h') {
+      showToast(t('settings.time_unit') + ': ' + (t('globals.minute_plural')))
+    } else if (getUserSetting('timeunit') === 'm') {
+      showToast(t('settings.time_unit') + ': ' + (t('globals.day_plural')))
+    }
+  }
+
 })
